c85d62aa0c3e6d257c748e51eda27cc503ce883d
MAMP <mamp@lab1862m.tumo.lab>
2019-05-06 11:31:10 +0400

    synth add

---
+++ synth/script.js
@@ -0,0 +1,36 @@
+
+//create a synth and connect it to the master output (your speakers)
+var synth = new Tone.Synth().toMaster()
+
+let freqArr = [261.62,293.66,311.12,329.62,349.22,369.99,391.99,440,466.16,493.88];
+
+$( "hr" ).mouseover(function() {
+       Tone.context.resume().then(() => {
+        //play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease(freqArr[Math.floor(Math.random()*freqArr.length)], '64n');
+$("hr").css("background-image", "linear-gradient(to right, #6a11cb 0%, #2575fc 100%")
+
+$(this).css("background","#fff");
+      })
+});
+
+
+  
+
+var count_particles, stats, update;
+  stats = new Stats;
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild(stats.domElement);
+  count_particles = document.querySelector('.js-count-particles');
+  update = function() {
+    stats.begin();
+    stats.end();
+    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+    requestAnimationFrame(update);
+  };
+  requestAnimationFrame(update);
